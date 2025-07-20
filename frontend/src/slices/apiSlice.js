7d3aba608832aa1fd864406b0986a76045c7f3ce
sudhhp <soodi.hp@gmail.com>
2025-07-20 17:07:35 +0330

    Set backend baseUrl using VITE_BACKEND_URL

--- frontend/src/slices/apiSlice.js
+++ frontend/src/slices/apiSlice.js
@@ -1,7 +1,12 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-const baseQuery = fetchBaseQuery({ baseUrl: "" });
+
+const baseQuery = fetchBaseQuery({
+  baseUrl: import.meta.env.VITE_BACKEND_URL || "/",
+  credentials: "include", // this keeps cookies like auth token
+});
+
 export const apiSlice = createApi({
   baseQuery,
   tagTypes: ["User"],
-  endpoints: (builder) => ({}),
+  endpoints: () => ({}),
 });
