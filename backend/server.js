4c76a17b83bba9d1576fdd4913602697f2a2734e
sudhhp <soodi.hp@gmail.com>
2025-07-20 20:09:14 +0330

    Removed frontend serving from backend (handled separately)

--- backend/server.js
+++ backend/server.js
@@ -16,13 +16,13 @@ app.use(cookieParser());
 app.use("/api/users", userRoutes);
 if (process.env.NODE_ENV === "production") {
   const __dirname = path.resolve();
-  app.use(express.static(path.join(__dirname, "frontend/dist")));
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
-  );
-} else {
-  app.get("/", (req, res) => res.send("server is ready"));
-}
+  // app.use(express.static(path.join(__dirname, "frontend/dist")));
+//   app.get("*", (req, res) =>
+//     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+//   );
+// } else {
+//   app.get("/", (req, res) => res.send("server is ready"));
+// }
 app.use(NotFound);
 app.use(ErrorHandler);
 app.listen(port, () => console.log(`Server is running on port ${port}`));
