5c1ba5550803fff21cc52975c556c75bd100f91b
sudhhp <soodi.hp@gmail.com>
2025-07-20 20:18:09 +0330

    Fix server.js for production build path on Render

--- backend/server.js
+++ backend/server.js
@@ -14,15 +14,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use("/api/users", userRoutes);
-if (process.env.NODE_ENV === "production") {
-  const __dirname = path.resolve();
-  // app.use(express.static(path.join(__dirname, "frontend/dist")));
-  // app.get("*", (req, res) =>
-  //   res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
-  // );
-} else {
-  app.get("/", (req, res) => res.send("server is ready"));
-}
+// if (process.env.NODE_ENV === "production") {
+//   const __dirname = path.resolve();
+//   app.use(express.static(path.join(__dirname, "frontend/dist")));
+//   app.get("*", (req, res) =>
+//     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+//   );
+// } else {
+//   app.get("/", (req, res) => res.send("server is ready"));
+// }
 app.use(NotFound);
 app.use(ErrorHandler);
 app.listen(port, () => console.log(`Server is running on port ${port}`));
